0a04971f73f99ed17a47f19cd7eda8d4488958f6
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-01-28 11:08:48 -0300

    add node

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'); // Flamework do Servidor
+var app     = express(); // Váriavel do express
+var http    = require('http').Server(app); // Servidor em si
+var path    = require('path'); // Módulo para manipular caminhos
+var port    = process.env.PORT || 3000; // Porta do servidor
+
+// Mandando o Express rodar estáticamente a pasta client
+app.use(express.static(path.join(__dirname, 'PugRunner'))); 
+
+
+// Na rota principal, a página é carregada 
+app.get('/', function(req, res){
+  res.sendFile('/client/index.html', {root:'.'});
+});
+
+// Rodando o servidor na determinada porta
+http.listen(port);
