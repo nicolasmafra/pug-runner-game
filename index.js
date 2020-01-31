1f74965f7981cb799ba10d6a5c6742a310b5b976
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-01-31 13:20:18 -0300

    Atualizado arquivo index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var express = require('express'); // Flamework do Servidor
-var app     = express(); // Váriavel do express
-var http    = require('http').Server(app); // Servidor em si
-var path    = require('path'); // Módulo para manipular caminhos
-var port    = process.env.PORT || 3000; // Porta do servidor
+var path = require('path'); // Módulo para manipular caminhos
 
-// Mandando o Express rodar estáticamente a pasta client
-app.use(express.static(path.join(__dirname, 'src'))); 
+var app = express(); // Variável do express
+var port = process.env.PORT || 3000; // Porta do servidor
 
-
-// Na rota principal, a página é carregada 
-app.get('/', function(req, res){
-  res.sendFile('/client/index.html', {root:'.'});
-});
+// Mandando o Express rodar estaticamente a pasta src
+app.use(express.static('src')); 
 
 // Rodando o servidor na determinada porta
-http.listen(port);
+app.listen(port);
